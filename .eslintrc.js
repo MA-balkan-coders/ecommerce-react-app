494bb06b378474f11610ad870c03d0e489e1fcf5
Ilija Pejanović <ikadvm96@gmail.com>
2023-04-23 15:14:54 +0200

    cleaned up

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
+  rules: {
+    'max-len': ['error', {
+      ignoreTemplateLiterals: true,
+      ignoreComments: true,
+    }],
+    'jsx-a11y/label-has-associated-control': ["error", {
+      assert: "either",
+    }],
+    'react/jsx-filename-extension': [1, {
+      'extensions': ['.js', '.jsx', '.ts', '.tsx']
+    }],
+  },
+};
